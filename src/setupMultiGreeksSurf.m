933c1805638e1d536e9fb04a1a56d70a6f498a78
Jerrod Estell <jestell@gcpud.org>
2024-04-18 10:14:44 -0700

    prog

--- src/setupMultiGreeksSurf.m
+++ src/setupMultiGreeksSurf.m
@@ -1,6 +1,6 @@
 function setupMultiGreeksSurf
     % Set up the GUI for visualizing multiple Greeks
-    fig = figure('Name', 'Multi-Greeks Visualization', 'Position', [100, 100, 1200, 500], 'NumberTitle', 'off', 'MenuBar', 'none');
+    fig = figure('Name', 'Multi-Greeks Visualization', 'Position', [100, 100, 1200, 700], 'NumberTitle', 'off', 'MenuBar', 'none');
 
     % Define parameters and their default values
     params = struct('StrikePrice', 100, 'RiskFreeRate', 0.05, 'Volatility', 0.20);
@@ -11,40 +11,42 @@ function setupMultiGreeksSurf
     limits = struct('StrikePrice', [50, 150], 'RiskFreeRate', [0.01, 0.1], 'Volatility', [0.1, 0.5]);
 
     % Setup sliders, labels, and value displays
+    baseY = 630; % Starting vertical position for sliders
     for i = 1:numParams
         paramName = paramNames{i};
-        uicontrol('Style', 'text', 'Parent', fig, 'Position', [10, 460 - i*30, 120, 20], ...
+        uicontrol('Style', 'text', 'Parent', fig, 'Position', [10, baseY - i*30, 120, 20], ...
                   'String', paramName, 'HorizontalAlignment', 'left');
-        slider = uicontrol('Style', 'slider', 'Parent', fig, 'Position', [130, 460 - i*30, 240, 20], ...
+        slider = uicontrol('Style', 'slider', 'Parent', fig, 'Position', [130, baseY - i*30, 240, 20], ...
                   'Min', limits.(paramName)(1), 'Max', limits.(paramName)(2), 'Value', params.(paramName), ...
                   'Tag', paramName, 'Callback', @updateGreeksPlots);
-        % Display for current value next to each slider
-        uicontrol('Style', 'text', 'Parent', fig, 'Position', [380, 460 - i*30, 50, 20], ...
+        uicontrol('Style', 'text', 'Parent', fig, 'Position', [380, baseY - i*30, 50, 20], ...
                   'String', num2str(get(slider, 'Value'), '%.2f'), 'Tag', [paramName 'Value'], 'HorizontalAlignment', 'left');
     end
 
     % Dropdown for option type selection
-    optionTypeDropdown = uicontrol('Style', 'popupmenu', 'Parent', fig, 'Position', [10, 400, 100, 20], ...
+    optionTypeDropdown = uicontrol('Style', 'popupmenu', 'Parent', fig, 'Position', [10, baseY - (numParams+1)*30, 100, 20], ...
+                                   'String', {'Call', 'Put'}, 'Tag', 'optionTypeDropdown', 'Callback', @updateGreeksPlots);
+
+
+    % Dropdown for option type selection
+    uicontrol('Style', 'popupmenu', 'Parent', fig, 'Position', [10, baseY - (numParams+1)*30, 100, 20], ...
                                    'String', {'Call', 'Put'}, 'Callback', @updateGreeksPlots);
 
     % Create axes for each Greek
-    axDelta = axes('Parent', fig, 'Position', [0.05, 0.1, 0.3, 0.8]);
-    title(axDelta, 'Delta Surface Plot');
+    axDelta = axes('Parent', fig, 'Position', [0.05, 0.1, 0.28, 0.5]);
+    title(axDelta, 'Delta');
     xlabel(axDelta, 'Stock Price');
     ylabel(axDelta, 'Time to Maturity');
-    zlabel(axDelta, 'Delta');
 
-    axGamma = axes('Parent', fig, 'Position', [0.37, 0.1, 0.3, 0.8]);
-    title(axGamma, 'Gamma Surface Plot');
+    axGamma = axes('Parent', fig, 'Position', [0.36, 0.1, 0.28, 0.5]);
+    title(axGamma, 'Gamma');
     xlabel(axGamma, 'Stock Price');
     ylabel(axGamma, 'Time to Maturity');
-    zlabel(axGamma, 'Gamma');
 
-    axTheta = axes('Parent', fig, 'Position', [0.69, 0.1, 0.3, 0.8]);
-    title(axTheta, 'Theta Surface Plot');
+    axTheta = axes('Parent', fig, 'Position', [0.67, 0.1, 0.28, 0.5]);
+    title(axTheta, 'Theta');
     xlabel(axTheta, 'Stock Price');
     ylabel(axTheta, 'Time to Maturity');
-    zlabel(axTheta, 'Theta');
 
     % Initial plot update
     updateGreeksPlots(fig, []);
@@ -60,23 +62,25 @@ function setupMultiGreeksSurf
         end
         optionType = get(optionTypeDropdown, 'String');
         selectedOptionType = optionType{get(optionTypeDropdown, 'Value')};
-    
+
         % Define the range of stock prices and time to maturity
         stockPrices = linspace(50, 150, 100);
         timeToMaturity = linspace(0.1, 2, 100);
-    
+
         % Prepare a meshgrid for the variables
         [S, T] = meshgrid(stockPrices, timeToMaturity);
-    
+
         % Calculate Greek values across the grid
         DeltaValues = arrayfun(@(s, t) mydelta(s, params.StrikePrice, t, params.RiskFreeRate, params.Volatility, selectedOptionType), S, T);
         GammaValues = arrayfun(@(s, t) mygamma(s, params.StrikePrice, t, params.RiskFreeRate, params.Volatility), S, T);
         ThetaValues = arrayfun(@(s, t) mytheta(s, params.StrikePrice, t, params.RiskFreeRate, params.Volatility, selectedOptionType), S, T);
-    
-        % Update surf plots
-        surf(axDelta, S, T, DeltaValues, 'EdgeColor', 'none', 'FaceColor', 'r');
-        surf(axGamma, S, T, GammaValues, 'EdgeColor', 'none', 'FaceColor', 'g');
-        surf(axTheta, S, T, ThetaValues, 'EdgeColor', 'none', 'FaceColor', 'b');
-    end
 
+        % Update surf plots
+        surf(axDelta, S, T, DeltaValues, 'EdgeColor', 'none', 'FaceAlpha', 0.7);
+        colormap(axDelta, 'winter');  % Use a different colormap for each plot for clarity
+        surf(axGamma, S, T, GammaValues, 'EdgeColor', 'none', 'FaceAlpha', 0.7);
+        colormap(axGamma, 'autumn');
+        surf(axTheta, S, T, ThetaValues, 'EdgeColor', 'none', 'FaceAlpha', 0.7);
+        colormap(axTheta, 'cool');
+    end
 end
